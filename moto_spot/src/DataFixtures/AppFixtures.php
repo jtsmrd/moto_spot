9dd57e12931cc766f4573793892708e279d9e4b7
JT Smrdel <jtsmrdel@gmail.com>
2021-02-13 18:14:57 -0500

    Added data fixtures for rider checkins

--- moto_spot/src/DataFixtures/AppFixtures.php
+++ moto_spot/src/DataFixtures/AppFixtures.php
@@ -2,15 +2,38 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\RiderCheckin;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class AppFixtures extends Fixture
 {
+    /**
+     * @var \Faker\Factory
+     */
+    private $faker;
+
+    public function __construct()
+    {
+        $this->faker = \Faker\Factory::create();
+    }
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $this->loadRiderCheckins($manager, 5000);
+    }
+
+    public function loadRiderCheckins(ObjectManager $manager, int $numRiderCheckins)
+    {
+        for ($i = 0; $i < $numRiderCheckins; $i++) {
+            $riderCheckin = new RiderCheckin();
+            $riderCheckin->setUserUUID($this->faker->uuid);
+            $riderCheckin->setLat($this->faker->latitude);
+            $riderCheckin->setLon($this->faker->longitude);
+            $riderCheckin->setCreateDate(new \DateTime());
+
+            $manager->persist($riderCheckin);
+        }
 
         $manager->flush();
     }
