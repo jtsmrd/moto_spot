93cc637790130836ede182f62f8f62c777a18683
JT Smrdel <jtsmrdel@gmail.com>
2021-02-14 13:00:29 -0500

    Readded default controller and twig file

--- moto_spot/src/Controller/DefaultController.php
+++ moto_spot/src/Controller/DefaultController.php
@@ -1,23 +1,20 @@
 <?php
 
-
 namespace App\Controller;
 
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/", name="default_index")
+     * @Route("/default", name="default")
      */
-    public function index()
+    public function index(): Response
     {
-        return new JsonResponse([
-            'action' => 'index',
-            'time' => time()
+        return $this->render('default/index.html.twig', [
+            'controller_name' => 'DefaultController',
         ]);
     }
 }
