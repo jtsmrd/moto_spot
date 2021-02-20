7d0b9b400664770ff486de20e721da88153b1bcc
JT Smrdel <jtsmrdel@gmail.com>
2021-02-20 15:27:51 -0500

    Display static test markers on map

--- moto_spot/assets/components/Map.js
+++ moto_spot/assets/components/Map.js
@@ -2,6 +2,29 @@ import React, { Component } from "react";
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
 
 export class MapContainer extends Component {
+  onDragEnd(mapProps, map, e) {
+    let ne = map.getBounds().getNorthEast();
+    let sw = map.getBounds().getSouthWest();
+    console.log(ne.lat() + ";" + ne.lng());
+    console.log(sw.lat() + ";" + sw.lng());
+  }
+
+  coords = [
+    { id: 1, lat: 40.4406, lng: -79.9859 },
+    { id: 2, lat: 40.4506, lng: -79.9759 },
+    { id: 3, lat: 40.4506, lng: -79.9659 },
+  ];
+
+  displayMarkers() {
+    let markers = [];
+    this.coords.forEach((coord) => {
+      markers.push(
+        <Marker key={coord.id} position={{ lat: coord.lat, lng: coord.lng }} />
+      );
+    });
+    return markers;
+  }
+
   render() {
     return (
       <Map
@@ -12,14 +35,9 @@ export class MapContainer extends Component {
           lng: -79.9959,
         }}
         style={{ width: "100%", height: "100%" }}
+        onDragend={this.onDragEnd}
       >
-        <Marker onClick={this.onMarkerClick} name={"Current location"} />
-
-        <InfoWindow onClose={this.onInfoWindowClose}>
-          <div>
-            <h1>{"none"}</h1>
-          </div>
-        </InfoWindow>
+        {this.displayMarkers()}
       </Map>
     );
   }
