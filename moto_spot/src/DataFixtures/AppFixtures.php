54a2afb5c509190a1b94d28dc01465c3edd64244
JT Smrdel <jtsmrdel@gmail.com>
2021-06-24 06:50:38 -0400

    Added RiderMeetup AppFixture

--- moto_spot/src/DataFixtures/AppFixtures.php
+++ moto_spot/src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\RiderCheckin;
+use App\Entity\RiderMeetup;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -21,6 +22,7 @@ class AppFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         $this->loadRiderCheckins($manager, 100);
+        $this->loadRiderMeetups($manager, 10);
     }
 
     public function loadRiderCheckins(ObjectManager $manager, int $numRiderCheckins)
@@ -44,6 +46,35 @@ class AppFixtures extends Fixture
             $riderCheckin->setExpireDateDisplay($expireDate);
 
             $manager->persist($riderCheckin);
+        }
+
+        $manager->flush();
+    }
+
+    public function loadRiderMeetups(ObjectManager  $manager, int $numRiderMeetups)
+    {
+        for ($i = 0; $i < $numRiderMeetups; $i++) {
+            $riderMeetup = new RiderMeetup();
+            $riderMeetup->setUserUUID($this->faker->uuid);
+
+            $lat = $this->faker->randomFloat(12, 40.38362639665043, 40.497339166747636);
+            $lon = $this->faker->randomFloat(12, -80.20980444333259, -79.77145102402869);
+            $riderMeetup->setLat($lat);
+            $riderMeetup->setLng($lon);
+
+            $createDate = (new \DateTime('now', new \DateTimeZone('UTC')));
+            $riderMeetup->setCreateDate($createDate);
+
+            $meetupDate = (new \DateTime('now', new \DateTimeZone('UTC')));
+            $riderMeetup->setMeetupDate($meetupDate);
+            $riderMeetup->setMeetupTimestamp($meetupDate->getTimestamp());
+
+            $expireDate = new \DateTime('now', new \DateTimeZone('UTC'));
+            $expireDate = $expireDate->add(new \DateInterval('PT5H'));
+            $riderMeetup->setExpireDate($expireDate);
+            $riderMeetup->setExpireTimestamp($expireDate->getTimestamp());
+
+            $manager->persist($riderMeetup);
         }
 
         $manager->flush();
