cfc09fd9585873432e75998a68f314b56f10c7f8
JT Smrdel <jtsmrdel@gmail.com>
2021-07-16 08:03:52 -0400

    Added Sentry and fixed material ui createTheme import

--- moto_spot/webpack.config.js
+++ moto_spot/webpack.config.js
@@ -62,7 +62,11 @@ Encore
     .enableTypeScriptLoader()
 
     // uncomment if you use React
-    .enableReactPreset();
+    .enableReactPreset()
+
+    .addPlugin({
+        'process.env': dotenv.parsed,
+    });
 
 // uncomment to get integrity="..." attributes on your script & link tags
 // requires WebpackEncoreBundle 1.4 or higher
